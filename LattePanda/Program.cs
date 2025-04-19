d8b0cbd6cf50ffd3bf6029d736f815e34508508e
John Staveley <john.staveley@myglobalbridge.com>
2025-04-19 18:33:02 +0100

    Minor mods

--- LattePanda/Program.cs
+++ LattePanda/Program.cs
@@ -8,34 +8,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("LattePanda hardware integration");
+        Console.WriteLine("LattePanda hardware integration demo");
         Arduino arduino = new Arduino();
-        Console.WriteLine("Setup pins");
+        Console.WriteLine("Setting up pins");
         arduino.PinMode(9, Arduino.INPUT);   // Motion Sensor
-        arduino.PinMode(10, Arduino.OUTPUT); // Periodic LED
-        arduino.PinMode(11, Arduino.OUTPUT); // Motion LED
-        arduino.PinMode(1, Arduino.ANALOG);  // Hals eye
-        arduino.WireBegin(200);
-        Console.WriteLine("Starting loop");
+        arduino.PinMode(10, Arduino.OUTPUT); // Red LED
+        arduino.PinMode(11, Arduino.OUTPUT); // Green LED
+        arduino.DigitalWrite(10, Arduino.HIGH);
+        arduino.DigitalWrite(11, Arduino.HIGH);
+        arduino.WireBegin(500);
+        Console.WriteLine("Starting loop:");
         while (true)
         {
-            Console.WriteLine("Red");
-            arduino.DigitalWrite(10, Arduino.HIGH);
+            Console.Write(".");
+            arduino.DigitalWrite(11, Arduino.HIGH);
+            arduino.DigitalWrite(11, Arduino.HIGH);
             var input = arduino.DigitalRead(9);
             if (input == Arduino.HIGH)
             {
+                Console.WriteLine("");
                 Console.WriteLine("Motion detected");
-                arduino.DigitalWrite(11, Arduino.HIGH);
+                arduino.DigitalWrite(10, Arduino.HIGH);
+                arduino.DigitalWrite(10, Arduino.HIGH);
+                Thread.Sleep(2000);
             }
             else
             {
-                Console.WriteLine("No motion detected");
-                arduino.DigitalWrite(11, Arduino.LOW);
+                arduino.DigitalWrite(10, Arduino.LOW);
+                arduino.DigitalWrite(10, Arduino.LOW);
             }
-            Thread.Sleep(2000);
-            Console.WriteLine("Green");
-            arduino.DigitalWrite(10, Arduino.LOW);
-            Thread.Sleep(2000);
+            Thread.Sleep(500);
+            arduino.DigitalWrite(11, Arduino.LOW);
+            arduino.DigitalWrite(11, Arduino.LOW);
+            Thread.Sleep(500);
         }
     }
 }
--- LattePanda/README.md
+++ LattePanda/README.md
@@ -1,5 +1,10 @@
 
 Deploy a Firmata sketch to a LattePanda first. See here: http://docs.lattepanda.com/content/1st_edition/vs_programming/#step-2-set-up-the-arduino
-Attach Red LED to pin 10 and Green LED to pin 11
+Attach Red LED to pin 10 and Green LED to pin 11, attach GND of LEDS to GND of LattePanda.
+Attach Motion sensor to pin 9, attach GND of motion sensor to GND of LattePanda, +ve to 5V. See component here: https://thepihut.com/products/pir-motion-sensor-module for more details
 Arduino.cs source code is available from 
 	https://github.com/LattePandaTeam/LattePanda-Development-Support/blob/master/LattePandaFirmata/Arduino.cs
+
+TODO: 
+- Add slides to github repo
+- Include some specs of the lattepanda
